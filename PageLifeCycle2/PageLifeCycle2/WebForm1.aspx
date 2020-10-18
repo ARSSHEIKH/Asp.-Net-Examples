73a0b89c90a53d17f250c8be89eabcdd355fc3dd
Arsalan <arsheikh665@gmail.com>
2020-10-19 02:01:54 +0500

    added filies

---
+++ PageLifeCycle2/PageLifeCycle2/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="PageLifeCycle2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Client Side Controls</h1>
+        <div>
+            <input type="text" />
+            <input type="radio" />
+            <input type="checkbox" />
+            <input type="button" value="Button"/>
+        </div>
+        <h1>Server Side Controls</h1>
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:RadioButton ID="RadioButton1" runat="server" />
+            <asp:CheckBox ID="CheckBox1" runat="server" />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>
+        
+
+    </form>
+</body>
+</html>
---
+++ PageLifeCycle2/PageLifeCycle2/WebForm1.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PageLifeCycle2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected override void OnPreInit(EventArgs e)
+        {
+            Response.Write("<br/> Inside OnPreInit Method");
+            base.OnPreInit(e);
+        }
+        protected override void OnInit(EventArgs e)
+        {
+            Response.Write("<br/> Inside OnInit Method");
+            base.OnInit(e);
+        }
+        protected override void OnInitComplete(EventArgs e)
+        {
+            Response.Write("<br/> Inside OnInitComplete Method");
+            base.OnInitComplete(e);
+        }
+        protected override void OnPreLoad(EventArgs e)
+        {
+            Response.Write("<br/> Inside OnPreLoad Method");
+            base.OnPreLoad(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Response.Write("<br/> Inside OnLoad Method");
+            base.OnLoad(e);
+        }
+        protected override void OnLoadComplete(EventArgs e)
+        {
+            Response.Write("<br/> Inside OnLoadComplete Method");
+            base.OnLoadComplete(e);
+        }
+        protected override void OnPreRender(EventArgs e)
+        {
+            Response.Write("<br/> Inside OnPreRender Method");
+            base.OnPreRender(e);
+        }
+        protected override void OnPreRenderComplete(EventArgs e)
+        {
+            Response.Write("<br/> Inside OnPreRenderComplete Method");
+            base.OnPreRenderComplete(e);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("<br/> Inside Page_Load Method");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("<br/> Inside Button1_Click Method");
+
+        }
+    }
+}
